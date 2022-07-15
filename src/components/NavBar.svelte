7bfc90678b60171c04f43ee7a241049d72ee3df0
Matthew Kaus <matthewkausfox@gmail.com>
2022-07-14 20:21:18 -0600

    set up components and pages folder

---
+++ src/components/NavBar.svelte
@@ -0,0 +1,27 @@
+<script></script>
+
+<div>
+    <div class="navbar bg-base-100">
+        <div class="navbar-start">
+            <a class="btn btn-ghost normal-case text-xl">Stock WatchList</a>
+        </div>
+        <div class="navbar-center">
+            <div class="form-control">
+                <input
+                    type="text"
+                    placeholder="Search"
+                    class="input input-bordered"
+                />
+            </div>
+        </div>
+        <div class="navbar-end">
+            <ul class="menu menu-horizontal p-0">
+                <li><a>Item 1</a></li>
+                <li><a>Item 2</a></li>
+                <li><a>Item 3</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<style></style>
---
+++ src/pages/Dashboard.svelte
@@ -0,0 +1,11 @@
+<script>
+    import NavBar from "../components/NavBar.svelte";
+    import Footer from "../components/Footer.svelte";
+</script>
+
+<div>
+    <NavBar />
+    <Footer />
+</div>
+
+<style></style>
