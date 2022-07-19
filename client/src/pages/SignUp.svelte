5594acc3a6139b869a4b7b89284c757770400743
Matthew Kaus <matthewkausfox@gmail.com>
2022-07-19 04:42:54 -0600

    added queries and mutaions. added utils folder in client, added jwt-decode v2.2.0

--- client/src/pages/SignUp.svelte
+++ client/src/pages/SignUp.svelte
@@ -30,6 +30,18 @@
 
                     <div class="form-control">
                         <label class="label">
+                            <span class="label-text">Username</span>
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="username"
+                            class="input input-bordered"
+                            id="username"
+                        />
+                    </div>
+
+                    <div class="form-control">
+                        <label class="label">
                             <span class="label-text">Email</span>
                         </label>
                         <input
